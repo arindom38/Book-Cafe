1a9a20d42b8873b2c6cf71e9b1148518ecd4c61d
ujjal.kundu <arindomkundu18@gmail.com>
2021-06-20 15:50:19 +0600

    routes for author is added

--- routes/authors.js
+++ routes/authors.js
@@ -1,19 +1,52 @@
 const express = require('express')
 const router = express.Router()
 const Author = require('../models/authorsModel')
+
 //get all the authors
-router.get('/',(req,res)=>{
-    res.render('authors/index')
+// URl = /authors ,method : get
+router.get('/',async (req,res)=>{
+    let searchToken = {}
+    //when user click 'authors' link req query will be empty so searchtoken is blank so all the authors will be shown
+    //when user search for a author, a form submit event (get request with a qery string pass and show the realed user onl)
+    if(req.query.name != null && req.query.name !== ''){
+        //regex used serach for both upper and lower case . i indicate case insensitive
+        searchToken.name = new RegExp(req.query.name,'i')
+    }
+    await Author.find(searchToken)
+        .then((result)=>{
+            res.render('authors/index',{
+                authors:result,
+                searchToken: req.query
+            })
+        })
+        .catch(()=>{
+            res.redirect('/')
+        })
 })
 
 //New author route
-router.get('/new',(req,res)=>{
-    res.render('authors/new',{author: new Author()})
+// URl = /authors/new ,method : post
+router.get('/new',async(req,res)=>{
+    res.render('authors/new',{author: new Author()}) //while rendering the file the model is also available in hte ejs
 })
 
 //create new author
-router.post('/',(req,res)=>{
-    res.render('create')
+// URl = /authors ,method : post
+router.post('/',async (req,res)=>{ //make the call back async will not block the code below this code
+    const author = new Author({
+        name: req.body.name
+    })
+    author.save()
+        .then((result)=>{
+            //res.redirect(`authors/${newAuthor:id}`)
+            res.redirect('authors')
+        })
+        .catch((err)=>{
+            res.render('authors/new',{ //if errro in creating new author the user should see the error in the same page
+                author: author,
+                errorMssg: 'Error Creating Author'
+            })
+        })
 })
 
 module.exports = router
