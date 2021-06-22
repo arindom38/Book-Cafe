e56626a41075213f8b9bdd9a8ad0a7399a9d7026
ujjal.kundu <arindomkundu18@gmail.com>
2021-06-22 11:59:36 +0600

    file upload handling added and modified

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,65 +1,64 @@
 const Book = require('../models/booksModel')
 const Author = require('../models/authorsModel')
+const fs = require('fs')
 
 
 
-const allBook = async (req,res)=>{
+const allBook = async (req, res) => {
     res.send('all books')
 }
 
-const newBook_get = async (req,res)=>{
-    console.log("New page request")
-    const book = new Book()
-    Author.find() //fetch all the authors and rendet to new.esj page
-        .then((result)=>{
-            res.render('books/new',{
-                book: book,
-                authors: result
-            })
-        })
-        .catch(()=>{
-            res.redirect('/books')
-        })
-    //renderNewPage(res, new Book())
+const newBook_get = async (req, res) => {
+    //passing new book cause we want to create one after filling the form
+    renderNewPage(res, new Book())
 }
 
-const newBook_post = async (req,res)=>{
-    //const fileName = req.file != null ? req.file.fileName : null
-    console.log(req.file)
+const newBook_post = async (req, res) => {
+    const filename = req.file != null ? req.file.filename : null
     const book = new Book({
         title: req.body.title,
         author: req.body.author,
         publishDate: new Date(req.body.publishDate),
         pageCount: Number(req.body.pageCount),
-        coverImageName: fileName,
+        coverImageName: filename,
         description: req.body.description
     })
-    console.log(book)
-   await book.save()
-        .then((result)=>{
-             //res.redirect(`books/${newBook:id}`)
-             res.redirect('/books')
+    await book.save()
+        .then((result) => {
+            //res.redirect(`books/${newBook:id}`)
+            res.redirect('/books')
         })
-        .catch((err)=>{
-            console.error(err)
-            res.send('Fail to save')
-           // renderNewPage(res, book, true)
+        .catch((err) => {
+            console.log(req.file)
+            if (book.coverImageName != null) {
+                removeCoverPage(req.file.path)
+            }
+            //if any error occur in creating it will pass the existing book
+            renderNewPage(res, book, true)
+
         })
 }
 
+async function removeCoverPage(filepath) {
+    fs.unlink(filepath, err => {
+        if (err) console.log(err)
+    })
+}
+
 async function renderNewPage(res, book, hasError = false) {
-    try {
-      const authors = await Author.find({})
-      const params = {
-        authors: authors,
-        book: book
-      }
-      if (hasError) params.errorMessage = 'Error Creating Book'
-      res.render('books/new', params)
-    } catch {
-      res.redirect('/books')
-    }
-  }
+    await Author.find()
+        .then((result) => {
+            const params = {
+                authors: result,
+                book: book
+            }
+            if (hasError) params.errorMssg = "Error in Creating Book"
+            res.render('books/new', params)
+        })
+        .catch(err => {
+            res.redirect('/books')
+        })
+}
 
 module.exports = {
     allBook,
