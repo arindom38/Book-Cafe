b6028f59a9e5cb2d566953b2bf3a76526f91cc82
ujjal.kundu <arindomkundu18@gmail.com>
2021-06-23 12:17:55 +0600

    Constrains for author remove with book added

--- models/authorsModel.js
+++ models/authorsModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Book = require('./booksModel')
 //creating schema
 const Schema = mongoose.Schema
 const authorSchema = new Schema({
@@ -7,6 +8,22 @@ const authorSchema = new Schema({
         required: true
     }
 },{timestamps: true})
+
+//constraint before removing author (check associcate books)
+authorSchema.pre('remove',function(next){
+    // executes, passing results to callback (books)
+    Book.find({ author: this.id},(err,books)=>{
+        if(err){ //if any db connect error
+            next(err)
+        }
+        else if(books.length > 0){ // if the requested author has books
+            next(new Error('This Author has Books still'))
+        }
+        else{ //else do nothing
+            next()
+        }
+    })
+})
 
 //wrap the shcema wiith model object and exports
 // (table/collection name, scheman name)
