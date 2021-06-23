b6237a851b8317e8a7eae3fe2ffc02cde636c3f7
Arindom Kundu <arindomkundu18@gmail.com>
2021-06-23 08:54:19 +0600

    author updating router added

--- routes/authors.js
+++ routes/authors.js
@@ -1,16 +1,33 @@
 const express = require('express')
 const router = express.Router()
 const authorController = require('../controllers/authorController')
+
 //get all the authors
 // URl = /authors ,method : get
 router.get('/',authorController.allAuthors)
 
 //New author route
-// URl = /authors/new ,method : post
+// URl = /authors/new ,method : get
 router.get('/new',authorController.newAuthor_get)
 
 //Search author
 // URl = /authors ,method : post
 router.post('/',authorController.newAuthor_post)
+
+//Show author
+// URl = /authors/:id ,method : get
+router.get('/:id',authorController.showAuthor_get)
+
+//Edit author 
+// URl = /authors/:id/edit ,method : get
+router.get('/:id/edit',authorController.editAuthor_get)
+
+//update author
+// URl = /authors/:id ,method : put
+router.put('/:id',authorController.updateAuthor_put)
+
+//Delete author
+// URl = /authors/:id ,method : delete
+router.delete('/:id',authorController.dltAuthor_delete)
 
 module.exports = router
