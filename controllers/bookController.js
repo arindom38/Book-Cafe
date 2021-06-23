4d10e43ab5f0f6dae3d65d462a17c76d4190104c
ujjal.kundu <arindomkundu18@gmail.com>
2021-06-23 16:48:44 +0600

    book edit, update and delt added

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,26 +1,26 @@
 const Book = require('../models/booksModel')
 const Author = require('../models/authorsModel')
-const imageMimeTypes = ['image/jpeg','image/png','image/gif']
+const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
 
 
 
 const allBook = async (req, res) => {
     //custom query for mongoDB
     let query = {}
-    if(req.query.title != null && req.query.title !== ''){
+    if (req.query.title != null && req.query.title !== '') {
         //regex used serach for both upper and lower case . i indicate case insensitive
         //query.function(model_propname, req.quer.proper)
-        query.title = new RegExp(req.query.title,'i')
+        query.title = new RegExp(req.query.title, 'i')
     }
-    if(req.query.publishbefore != null && req.query.publishbefore != ''){
-        query.publishDate = {'$lte': req.query.publishbefore } // lte is <= operator for mongodb
+    if (req.query.publishbefore != null && req.query.publishbefore != '') {
+        query.publishDate = { '$lte': req.query.publishbefore } // lte is <= operator for mongodb
     }
-    if(req.query.publishafter != null && req.query.publishafter != ''){
-        query.publishDate  = {'$gte': req.query.publishafter }// gte is => operator mongodb
+    if (req.query.publishafter != null && req.query.publishafter != '') {
+        query.publishDate = { '$gte': req.query.publishafter }// gte is => operator mongodb
     }
     await Book.find(query) //pass the custom query
         .then((result) => {
-            res.render('books/index',{
+            res.render('books/index', {
                 books: result,
                 searchToken: req.query
             })
@@ -43,12 +43,11 @@ const newBook_post = async (req, res) => {
         pageCount: Number(req.body.pageCount),
         description: req.body.description
     })
-    saveCover(book,req.body.cover)
+    saveCover(book, req.body.cover)
 
     await book.save()
         .then((result) => {
-            //res.redirect(`books/${newBook:id}`)
-            res.redirect('/books')
+            res.redirect(`books/${result.id}`)
         })
         .catch((err) => {
             console.log(req.file)
@@ -57,32 +56,102 @@ const newBook_post = async (req, res) => {
         })
 }
 
-// async function removeCoverPage(filepath) {
-//     fs.unlink(filepath, err => {
-//         if (err) console.log(err)
-//     })
-// }
+const showBook_get = async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id).populate('author').exec()
+        res.render('books/show', { book: book })
+    } catch (error) {
+        res.redirect('/')
+    }
+}
+
+
+const editBook_get = async (req, res) => {
+    await Book.findById(req.params.id)
+        .then((result) => {
+            renderEditPage(res, result, false)
+        })
+        .catch(err => {
+            res.redirect(`/books/${req.params.id}`)
+        })
+}
+
+const updateBook_put = async (req, res) => {
+    await Book.findById(req.params.id)
+        .then((result) => {
+            result.title = req.body.title
+            result.author = req.body.author
+            result.pageCount = Number(req.body.pageCount)
+            result.description = req.body.description
+            result.publishDate = new Date(req.body.publishDate)
+
+            if (req.body.cover != null && req.body.cover != '') {
+                saveCover(book, req.body.cover)
+            }
+            result.save()
+                .then((resultsave) => {
+                    res.redirect(`/books/${resultsave.id}`)
+                })
+                .catch((err) => {
+                    console.log(err)
+                    renderEditPage(res, book, true)
+                })
+        })
+        .catch(err => {
+            res.redirect('/')
+        })
+}
+
+const dltBook_delete = async (req, res) => {
+    await Book.findById(req.params.id)
+        .then((resultfind) => {
+            resultfind.remove()
+                .then((result) => {
+                    res.redirect('/books')
+                })
+                .catch((err) => {
+                    res.redirect(`/books/${resultfind.id}`)
+                })
+        })
+        .catch((err) => { //when author is not found
+            res.redirect('/') //redirect to home page
+        })
+}
 
 async function renderNewPage(res, book, hasError = false) {
+    renderFormPage(res, book, 'new', hasError)
+}
+
+async function renderEditPage(res, book, hasError = false) {
+    renderFormPage(res, book, 'edit', hasError)
+}
+
+async function renderFormPage(res, book, form, hasError = false) {
     await Author.find()
         .then((result) => {
             const params = {
                 authors: result,
                 book: book
             }
-            if (hasError) params.errorMssg = "Error in Creating Book"
-            res.render('books/new', params)
+            if (hasError) {
+                if (form === 'edit') {
+                    params.errorMssg = "Error in Editing Book"
+                } else {
+                    params.errorMssg = "Error in Creating Book"
+                }
+            }
+            res.render(`books/${form}`, params)
         })
         .catch(err => {
             res.redirect('/books')
         })
 }
 
-function saveCover(book,coverEncoded){
-    if(coverEncoded == null) return
+function saveCover(book, coverEncoded) {
+    if (coverEncoded == null) return
     const cover = JSON.parse(coverEncoded)
-    if(cover != null && imageMimeTypes.includes(cover.type) ){
-        book.coverImage = new Buffer.from(cover.data,'base64')
+    if (cover != null && imageMimeTypes.includes(cover.type)) {
+        book.coverImage = new Buffer.from(cover.data, 'base64')
         book.coverImageType = cover.type
     }
 }
@@ -90,5 +159,9 @@ function saveCover(book,coverEncoded){
 module.exports = {
     allBook,
     newBook_get,
-    newBook_post
+    newBook_post,
+    showBook_get,
+    editBook_get,
+    updateBook_put,
+    dltBook_delete
 }
--- routes/books.js
+++ routes/books.js
@@ -23,8 +23,25 @@ router.get('/',bookController.allBook)
 // URl = /books/new ,method : get
 router.get('/new',bookController.newBook_get)
 
+
 //Create book
 // URl = /books ,method : post
 router.post('/',bookController.newBook_post)
+
+//show book route
+// URl = /books/:id ,method : get
+router.get('/:id',bookController.showBook_get)
+
+//Edit book route
+// URl = /books/:id ,method : get
+router.get('/:id/edit',bookController.editBook_get)
+
+//Update book route
+// URl = /books/:id ,method : get
+router.put('/:id',bookController.updateBook_put)
+
+//Delete book route
+// URl = /books/:id ,method : get
+router.delete('/:id',bookController.dltBook_delete)
 
 module.exports = router
