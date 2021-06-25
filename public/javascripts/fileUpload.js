85d4522ff3eed9f8b0d5121b6cfaa6dec8515566
Arindom Kundu <arindomkundu18@gmail.com>
2021-06-26 00:01:36 +0600

    file upload styles enable

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,3 +1,19 @@
+//all css are loaded after this js then main css then css import into main.css
+// no css will affect  where filepond is applied
+//so we need to manually import the stylesheets
+const rootStyles = window.getComputedStyle(document.documentElement) //all root styles will be available
+if(rootStyles.getPropertyValue('--book-cover-width-large') != null && rootStyles.getPropertyValue('--book-cover-width-large') != ''){ //when the property is not loaded
+    ready() //call filepond plugin
+}else{
+    //if not loaded then load main-css by id and then call ready function
+    document.getElementById('main-css').addEventListener('load',ready)
+}
+
+function ready(){
+    //getting the values dynamically from root styles
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+    const coverAspectRatio =parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+    const coverHeight = coverWidth / coverAspectRatio
 // Register the plugin
 FilePond.registerPlugin(
     FilePondPluginFileEncode,
@@ -6,8 +22,9 @@ FilePond.registerPlugin(
 )
 
 FilePond.setOptions({
-    stylePanelAspectRatio: 150/100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
+    stylePanelAspectRatio: 1 / coverAspectRatio,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeight
 })
 FilePond.parse(document.body)
+}
